daa7dabd8b5d30360497801e050348f757a89155
Yifan Ai <yifanai@aol.com>
2020-12-21 16:49:55 +1100

    Add more examples

--- example/src/App.js
+++ example/src/App.js
@@ -5,10 +5,15 @@ import 'react-responsive-button/dist/index.css';
 
 const App = () => {
   return (
-    <>
+    <div>
+      <Button>Default Button</Button>
       <Button disabled>Disabled Button</Button>
-      <Button>Normal Button</Button>
-    </>
+      <Button style={{ background: 'red' }}>Red Button</Button>
+      <Button style={{ boxShadow: '0 0 1rem 0 rgba(0, 0, 0, 0.5)' }}>
+        Button with shadow
+      </Button>
+      <Button style={{ border: '1px solid red' }}>Button with border</Button>
+    </div>
   );
 };
 
