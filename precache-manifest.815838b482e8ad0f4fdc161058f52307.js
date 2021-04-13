e82dd8e295b21faf867aec2b1202efe3292e8168
Yifan Ai <yifanai@aol.com>
2021-04-13 23:59:07 +1000

    Updates

---
+++ precache-manifest.815838b482e8ad0f4fdc161058f52307.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2588324fa2cb545f7e5c1baead5f9f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b777512a80c876b959cc",
+    "url": "./static/css/main.5979d461.chunk.css"
+  },
+  {
+    "revision": "d06e7e64b2f9b4d2328a",
+    "url": "./static/js/2.f7586a0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f7586a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b777512a80c876b959cc",
+    "url": "./static/js/main.8c883c2a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.8c883c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf6ed9eb7f97eb94e1c",
+    "url": "./static/js/runtime-main.64ed8db7.js"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "./static/media/favicon.c92b85a5.ico"
+  }
+]);
